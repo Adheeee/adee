f978a735eb8b00ee25ac08c75e1d4df25f176d20
adheeee <aadeindawan04@gmail.com>
2023-10-09 19:07:07 +0700

    ini adalah codingan adee

---
+++ Day1ade.java
@@ -0,0 +1,16 @@
+package day1ade;
+
+public class Day1ade {
+
+    public static void main(String[] args) {
+        String nama = "ade indawan";
+        int umur = 19;
+        String alamat = "lutang";
+        
+        System.out.println("nama :"+nama);
+        System.out.println("umur :"+umur);
+        System.out.println("alamat :"+alamat);
+
+    }
+
+}
