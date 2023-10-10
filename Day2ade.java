8630d2670ffa3a357905084af167465de75fa661
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-10 13:24:20 +0800

    Add files via upload

---
+++ Day2ade.java
@@ -0,0 +1,12 @@
+package day2ade;
+
+public class Day2ade {
+
+    public static void main(String[] args) {
+       String nama_depan ="ade";
+       String nama_belakang ="indawan";
+        System.out.println(nama_depan +" "+ nama_belakang);
+        
+    }
+
+}
